7de3ab956cc19a2f7fb203658dcf9ea0e37ce82b
Ram Kandaswamy <awsraman@amazon.com>
2023-11-08 23:18:02 +0000

    Testing

--- src/main/java/com/aws/proserve/ggcommons/config/manager/ConfigurationChangeListener.java
+++
@@ -1,7 +0,0 @@
-package com.aws.proserve.ggcommons.config.manager;
-
-public interface ConfigurationChangeListener
-{
-    // Implementations of onConfigurationChanged() should return true if the configuration was changed.
-    public boolean onConfigurationChanged();
-}
