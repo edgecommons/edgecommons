0e87ac2911d9852bdb1eb690367579378e713aa3
Marc Breissinger <mbreissi@amazon.com>
2025-02-28 09:58:54 -0500

    fix: standardized COMPONENTNAME symbol

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import argparse
 import logging
 
 import ggcommons
-from app.<<COMPONENT_NAME>> import <<COMPONENT_NAME>>
+from app.<<COMPONENTNAME>> import <<COMPONENTNAME>>
 
 logger = logging.getLogger("main")
 
@@ -11,14 +11,14 @@ def main():
     arg_parser = argparse.ArgumentParser()
     # add any component specific arguments here
     args, config_manager, heartbeat = ggcommons.init(
-        component_name="<<COMPONENT_NAME>>",
+        component_name="<<COMPONENTNAME>>",
         arg_parser=arg_parser,
         receive_own_messages=True,
     )
-    app = <<COMPONENT_NAME>>(args=args, config_manager=config_manager)
+    app = <<COMPONENTNAME>>(args=args, config_manager=config_manager)
     app.run()
 
 
 if __name__ == "__main__":
-    logger.info("Starting <<COMPONENT_NAME>>")
+    logger.info("Starting <<COMPONENTNAME>>")
     main()
