85d8c1058c3c098ef42a3b6105656e443cd15f34
Marc Breissinger <mbreissi@amazon.com>
2025-02-28 09:59:44 -0500

    fix: Removed incorrect comment

--- app/greengrass_app.py
+++ app/greengrass_app.py
@@ -10,13 +10,6 @@ from ggcommons.config.manager.config_manager import ConfigManager
 logger = logging.getLogger("<<COMPONENTNAME>>")
 
 
-# This sample application subscribes to messages on the topic "hello/world" and
-# then publishes a message every n seconds on that topic, where "n" comes from the
-# app specific configuration section in the config file/recipe.  The message is output
-# to the log.  The application inherits configuration management, heartbeats, logging
-# and switching between local MQTT and GG IPC from ggcommons.
-
-
 class <<COMPONENTNAME>>(ConfigurationChangeListener, ABC):
     def __init__(self, args: Namespace, config_manager: ConfigManager):
         super().__init__()
