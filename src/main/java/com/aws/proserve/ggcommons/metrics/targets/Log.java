06636e1bdf5f159dc4db1863b31160ff3e265719
Marc Breissinger <mbreissi@amazon.com>
2025-07-26 16:18:52 -0400

    Fixed file logger bug; added file rotation; updated documentation

--- src/main/java/com/aws/proserve/ggcommons/metrics/targets/Log.java
+++ src/main/java/com/aws/proserve/ggcommons/metrics/targets/Log.java
@@ -11,14 +11,7 @@ import com.google.gson.JsonObject;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.core.config.Configurator;
-import org.apache.logging.log4j.core.config.builder.api.*;
-import org.apache.logging.log4j.core.config.builder.impl.BuiltConfiguration;
-
 import java.util.Map;
-
-import static org.apache.logging.log4j.core.config.builder.api.ConfigurationBuilderFactory.newConfigurationBuilder;
-
 
 public class Log extends MetricTarget
 {
@@ -77,54 +70,6 @@ public class Log extends MetricTarget
         return true;
     }
 
-//    private JsonObject buildMetricData(Metric metric, Map<String, Float> measureValues, boolean largeFleetWorkaround) {
-//        JsonObject emfObject = new JsonObject();
-//
-//        JsonObject awsObject = new JsonObject();
-//        awsObject.addProperty("Timestamp", System.currentTimeMillis()/1000);
-//
-//        JsonArray cwMetricsArray = new JsonArray();
-//        JsonObject cwMetricsArrayEntry = getMetricsMetadata(metric);
-//        cwMetricsArray.add(cwMetricsArrayEntry);
-//        awsObject.add("CloudWatchMetrics", cwMetricsArray);
-//
-//        for (Map.Entry<String, String> entry : metric.getDimensions().entrySet())
-//        {
-//            if (largeFleetWorkaround && entry.getKey().equals("coreName"))
-//                emfObject.addProperty(entry.getKey(), "ALL");
-//            else
-//                emfObject.addProperty(entry.getKey(), entry.getValue());
-//        }
-//        for (Map.Entry<String, Float> entry : measureValues.entrySet())
-//            emfObject.addProperty(entry.getKey(), entry.getValue());
-//        emfObject.add("_aws", awsObject);
-//
-//        return emfObject;
-//    }
-//
-//    private JsonObject getMetricsMetadata(Metric metric)
-//    {
-//        JsonObject cwMetricsArrayEntry = new JsonObject();
-//        cwMetricsArrayEntry.addProperty("Namespace", metricConfig.getNamespace());
-//        JsonArray dimensionSetArray = new JsonArray();
-//        JsonArray dimensionArray = new JsonArray();
-//        for (Map.Entry<String, String> dimension : metric.getDimensions().entrySet())
-//            dimensionArray.add(dimension.getKey());
-//        dimensionSetArray.add(dimensionArray);
-//        cwMetricsArrayEntry.add("Dimensions", dimensionSetArray);
-//        JsonArray metricsMetadataArray = new JsonArray();
-//        for (Measure measure : metric.getMeasures().values())
-//        {
-//            JsonObject measureMetadataEntry = new JsonObject();
-//            measureMetadataEntry.addProperty("Name", measure.getName());
-//            measureMetadataEntry.addProperty("Unit", measure.getUnit());
-//            measureMetadataEntry.addProperty("StorageResolution", measure.getStorageResolution());
-//            metricsMetadataArray.add(measureMetadataEntry);
-//        }
-//        cwMetricsArrayEntry.add("Metrics", metricsMetadataArray);
-//        return cwMetricsArrayEntry;
-//    }
-
     private Logger configureMetricLogger()
     {
         MetricConfiguration metricConfig = configManager.getMetricConfig();
@@ -154,8 +99,8 @@ public class Log extends MetricTarget
                 org.apache.logging.log4j.core.appender.RollingFileAppender.newBuilder()
                     .withFileName(metricFile)
                     .withFilePattern(metricFile + ".%i")
-                    .withName("MetricFileAppender")
-                    .withLayout(layout)
+                    .setName("MetricFileAppender")
+                    .setLayout(layout)
                     .withPolicy(triggeringPolicy)
                     .withStrategy(rolloverStrategy)
                     .build();
@@ -165,19 +110,12 @@ public class Log extends MetricTarget
             
             // Create logger configuration for metrics
             org.apache.logging.log4j.core.config.LoggerConfig loggerConfig = 
-                org.apache.logging.log4j.core.config.LoggerConfig.createLogger(
-                    false, // additivity
-                    Level.INFO,
+                new org.apache.logging.log4j.core.config.LoggerConfig(
                     "metric",
-                    "true",
-                    new org.apache.logging.log4j.core.config.AppenderRef[] {
-                        org.apache.logging.log4j.core.config.AppenderRef.createAppenderRef(
-                            "MetricFileAppender", null, null)
-                    },
-                    null,
-                    config,
-                    null
+                    Level.INFO,
+                    false // additivity
                 );
+            loggerConfig.addAppender(appender, Level.INFO, null);
             
             config.addLogger("metric", loggerConfig);
             context.updateLoggers();
